56963e27a3da5b479d6a53c5fb5d646e7fd4b253
Edward Zhang <edward@zhang.nz>
2019-06-18 16:03:11 +1200

    Integration of UI into prototype scene

--- Assets/CustomUI/DSOperationControllers/ArrayOperationController.cs
+++ Assets/CustomUI/DSOperationControllers/ArrayOperationController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LevelManager;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +11,8 @@ namespace CustomUI.DSOperationControllers
         public InputField IndexInputField;
         public Text CodePreviewView;
         public ArrayOperations OperationType;
+        public ArrayLevelManager LevelManager;
+        public int NumberOfParks;
         
         // Start is called before the first frame update
         void Start()
@@ -20,24 +23,37 @@ namespace CustomUI.DSOperationControllers
         private void ProcessExecution()
         {
             if (IndexInputField.text.Length == 0)
+            {
                 CodePreviewView.text = "Invalid Index";
-            
+                return;
+            }
+
             var index = int.Parse(IndexInputField.text);
+
+            if (index >= NumberOfParks)
+            {
+                CodePreviewView.text = "Index out of range";
+                return;
+            }
+            
             switch (OperationType)
             {
                 case ArrayOperations.Add:
                     CodePreviewView.text = "car = new Car();\n" +
                                            $"array[{index}] = car;";
-                    // TODO: Trigger Array Operation Code
+                    LevelManager.Spawn(obj => { LevelManager.WriteToArray(obj, index);; });
                     break;
                 case ArrayOperations.ToTemp:
                     CodePreviewView.text = $"temp = array[{index}];";
+                    LevelManager.CopyFromIndexToTempVar(index);
                     break;
                 case ArrayOperations.FromTemp:
                     CodePreviewView.text = $"array[{index}] = temp;";
+                    LevelManager.CopyFromTempVarToIndex(index);
                     break;
                 case ArrayOperations.Delete:
                     CodePreviewView.text = $"array[{index}] = null;";
+                    LevelManager.Destroy(index);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
