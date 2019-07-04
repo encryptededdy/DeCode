b56c452ed939f588649c70c2b1b73cf72fc0ea11
Simon Su <f-7-beihan@hotmail.com>
2019-07-04 21:30:05 +1200

    Added background to array and list level

---
+++ Assets/Scripts/Misc/FullscreenSprite.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Misc
+{
+    public class FullscreenSprite : MonoBehaviour
+    {
+        void Awake()
+        {
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+            if (Camera.main != null)
+            {
+                float cameraHeight = Camera.main.orthographicSize * 2;
+                Vector2 cameraSize = new Vector2(Camera.main.aspect * cameraHeight, cameraHeight);
+                Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
+
+                Vector2 scale = transform.localScale;
+                if (cameraSize.x >= cameraSize.y)
+                {
+                    // Landscape (or equal)
+                    scale *= cameraSize.x / spriteSize.x;
+                }
+                else
+                {
+                    // Portrait
+                    scale *= cameraSize.y / spriteSize.y;
+                }
+
+                transform.position = Vector2.zero; // Optional
+                transform.localScale = scale;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
